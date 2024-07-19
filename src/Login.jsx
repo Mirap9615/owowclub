ca30aa7542ce4dc5803f3e312702c58d24f6c274
Mirap9615 <wc6972z2@gmail.com>
2024-07-19 10:26:56 -0700

    accounts

--- src/Login.jsx
+++ src/Login.jsx
@@ -9,16 +9,6 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const checkLoginStatus = async () => {
-      const isAuthenticated = await checkAuth();
-      if (isAuthenticated) {
-        navigate('/home');
-      }
-    };
-    checkLoginStatus();
-  }, [navigate]);
-
   const handleLogin = async (e) => {
     e.preventDefault();
     try {

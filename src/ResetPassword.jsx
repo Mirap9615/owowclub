c7272b735164f86bb9ea326d190257b1443b10ad
Mirap9615 <wc6972z2@gmail.com>
2024-10-05 20:50:52 -0400

    m

--- src/ResetPassword.jsx
+++ src/ResetPassword.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import './ResetPassword.css';
 
 const ResetPassword = () => {
   const { token } = useParams();
